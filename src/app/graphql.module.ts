c2e43d3b7357c946adcb9b00ffdb2dcc897c6e28
sipho hlophe <psyphore@gmail.com>
2019-12-24 16:52:28 +0200

    refactor: update apollo client configuration

--- src/app/graphql.module.ts
+++ src/app/graphql.module.ts
@@ -3,16 +3,17 @@ import { HttpHeaders } from '@angular/common/http';
 import { ApolloModule, APOLLO_OPTIONS } from 'apollo-angular';
 import { HttpLinkModule, HttpLink } from 'apollo-angular-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
+import { Apollo } from 'apollo-angular';
+import { ApolloLink, concat } from 'apollo-link';
 
-import { AuthService } from './shared/services/security.service';
-import { ApolloLink } from 'apollo-link';
+import { AuthService } from '@services/security.service';
 import { environment } from '@environments/environment';
 
 const uri = environment.graphQL_URI2; // <-- add the URL of the GraphQL server here
 const auth = new AuthService();
 
 
-export function createApollo(httpLink: HttpLink, apollo: ApolloLink) {
+export function createApollo(httpLink: HttpLink, apollo: Apollo) {
 const http = httpLink.create({
   uri,
   useMultipart: true
@@ -28,17 +29,14 @@ const authMiddleware = new ApolloLink((operation, forward) => {
   return forward(operation);
 });
 
+const cache = new InMemoryCache();
+
   apollo.create({
     link: concat(authMiddleware, http),
-    cache: new InMemoryCache()
+    cache
   });
 
   return apollo;
-
-  // return {
-  //   link: concat(authMiddleware, http),
-  //   cache: new InMemoryCache()
-  // };
 }
 
 @NgModule({
@@ -47,7 +45,7 @@ const authMiddleware = new ApolloLink((operation, forward) => {
     {
       provide: APOLLO_OPTIONS,
       useFactory: createApollo,
-      deps: [HttpLink, ApolloLink]
+      deps: [HttpLink, Apollo]
     }
   ]
 })
