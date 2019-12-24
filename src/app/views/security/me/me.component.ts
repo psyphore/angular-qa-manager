22d5790340936d852118d43078cfaa89f98bfdd7
sipho hlophe <psyphore@gmail.com>
2019-12-24 08:42:38 +0200

    feat: fetch user profile via GraphQL

--- src/app/views/security/me/me.component.ts
+++ src/app/views/security/me/me.component.ts
@@ -1,4 +1,9 @@
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { AuthService, StrapiAuthService, StrapiMeService } from '@shared/services';
+import { SignInCredentials } from '@shared/interfaces/security.interface';
 
 @Component({
   selector: 'app-me',
@@ -6,10 +11,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./me.component.css']
 })
 export class MeComponent implements OnInit {
+  public signInFormGroup: FormGroup;
+  public errorMessage: string = null;
+  public profile: Observable<any>;
 
-  constructor() { }
+  constructor(private meSvc: StrapiMeService, private authSvc: AuthService) { }
 
-  ngOnInit() {
+  async ngOnInit() {
+    this.errorMessage = null;
+    try{
+      if(this.authSvc.hasToken()) {
+
+      }
+      this.profile = await this.meSvc.watch(null, {
+        fetchPolicy: 'network-only'
+      })
+      .valueChanges
+      .pipe(
+        map(result => result.data.me)
+      );
+
+    }catch(err) {
+      console.error(err);
+      this.errorMessage = err.message;
+    }
+
   }
 
 }
