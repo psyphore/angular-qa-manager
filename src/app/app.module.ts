597857b0a21f7b3f10caed519a6c60772e3041f5
Sipho Hlophe <sipho.hlophe@betterlifegroup.co.za>
2020-08-17 10:37:00 +0200

    chore: update package references

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,7 @@ import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-
 import { HttpClientModule } from '@angular/common/http';
-import { ApolloModule } from 'apollo-angular';
-import { HttpLinkModule } from 'apollo-angular-link-http';
-
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
@@ -21,8 +17,6 @@ import { RootStoreModule } from '@root-store/root-store.module';
     BrowserModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    ApolloModule,
-    HttpLinkModule,
     RouterModule,
 
     SharedModule,
